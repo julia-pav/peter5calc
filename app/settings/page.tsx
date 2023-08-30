05d5890b15ae989df270c34ad9159ab5ff106703
Yuliia Pavliuk <olga96855@gmail.com>
2023-08-30 19:13:15 +0300

    fix bug

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -2,16 +2,17 @@ import Link from 'next/link';
 
 export default function Home() {
     return (
-      <div>
-        <h1>
-          Settings!
-        </h1>
-      </div>
-
-<Link href="/">
-<a>[Go to Home Page]</a>
-</Link>
-
-    )
+        <div>
+          <h1>
+            Settings!
+          </h1>
+        </div>
+     
+  
+      <Link href="/">
+      <a>[Go to Home Page]</a>
+    </Link>
+    
+   )
 
   }
