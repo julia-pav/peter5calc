fd12c36f1681976955d43e240ef426b64820faf1
Yuliia Pavliuk <olga96855@gmail.com>
2023-09-01 16:51:06 +0300

    update r1

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,25 @@
-// import Link from 'next/link';
+import Link from 'next/link';
 import React from 'react';
+
+// components/Layout.js
+
+import styles from './Layout.module.css';
+
+export default function Layout({ children }) {
+  return (
+    <div>
+      <nav className={styles.navbar}>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </nav>
+      <main>{children}</main>
+    </div>
+  );
+}
 
 
 export default function Home() {
--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,37 @@
 // import Link from 'next/link';
 import React from 'react';
 
+
+import Layout from '../components/Layout';
+
 export default function Home() {
-
-const url1 = '/dashboard';
-const url2 = '/settings'
   return (
-    <div>
-      <h1>
-        Hello!
-      </h1>
-   
-
-<a href={url1}>
-[GO TO DASHBOARD]
-</a>
-
-<a href={url2}>
-[GO TO SETTINGS]
-</a>
-
-
- </div>
+    <Layout>
+      <h1>Home Page</h1>
+    </Layout>
   );
 }
+
+// export default function Home() {
+
+// const url1 = '/dashboard';
+// const url2 = '/settings'
+//   return (
+//     <div>
+//       <h1>
+//         Hello!
+//       </h1>
+   
+
+// <a href={url1}>
+// [GO TO DASHBOARD]
+// </a>
+
+// <a href={url2}>
+// [GO TO SETTINGS]
+// </a>
+
+
+//  </div>
+//   );
+// }
