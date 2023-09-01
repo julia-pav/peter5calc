808e12ac8639b38264511bf225ff947ff3d86d48
Yuliia Pavliuk <olga96855@gmail.com>
2023-09-01 18:06:16 +0300

    fix bugs #1

--- app/components/layouts/Header.tsx
+++ app/components/layouts/Header.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import styles from './components/layouts/Layout.module.css';
+import styles from './components/layouts/navstyle';
 
 
 export default function Header() {
