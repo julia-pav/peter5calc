aaa5d14fbf4b5d5d355ec595a9c6af11446c5e64
Yuliia Pavliuk <olga96855@gmail.com>
2023-09-05 19:59:20 +0300

    Tried to add Chakra

--- app/components/layouts/Header.tsx
+++ app/components/layouts/Header.tsx
@@ -1,33 +1,23 @@
-import Link from 'next/link';
-// import styles from './components/layouts/navstyle.module.css';
-
-
+import styles from './components/layouts/navstyle.module.css'
 
 export default function Header() {
-  const url1 = '/dashboard';
-const url2 = '/settings'
-    return (
-        <div>
-
-        <nav >
-        <Link href="/">
-          <a>[Home]</a>
-        </Link>
-        
-   
-<a href={url1}>
-[GO TO DASHBOARD]
-</a>
-
-<a href={url2}>
-[GO TO SETTINGS]
-</a>
-</nav>
-
- </div>
-     
-
-    
-    
-    )
+	const url1 = '/dashboard'
+	const url2 = '/settings'
+	return (
+		<div>
+			<nav className={styles.navbar}>
+				<ul>
+					<li>
+						<a href='/'>Home</a>
+					</li>
+					<li>
+						<a href={url1}>GO TO DASHBOARD</a>
+					</li>
+					<li>
+						<a href={url2}>GO TO SETTINGS</a>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	)
 }
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,24 +1,14 @@
 // import Link from 'next/link';
-import React from 'react';
-
+import React from 'react'
 
 export default function Home() {
+	const myHome = '/'
 
-const myHome = '/';
+	return (
+		<div>
+			<h1>Dashboard!</h1>
 
-    return (
-      <div>
-        <h1>
-          Dashboard!
-        </h1>
-      
-   
-<a href={myHome}>
-[GO TO HOME]
-</a>
-  
-</div>
-  
-
- );
-  }
+			<a href={myHome}>[GO TO HOME]</a>
+		</div>
+	)
+}
