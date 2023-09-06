4ce58f3f9808166803de0c09ae62f8b383864621
Yuliia Pavliuk <olga96855@gmail.com>
2023-09-06 16:28:17 +0300

    test#3

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,24 @@
 import Link from 'next/link'
 import React from 'react'
-import { Box, Flex, Image, Container } from '@chakra-ui/react'
+
+import { Heading, Box, Divider, Flex, Image, Container } from '@chakra-ui/react'
 
 export default function Home() {
+	const url1 = '/dashboard'
+	const url2 = '/settings'
 	return (
 		<div>
-			<Box>
-				<h1>Hello! IT IS HOME!</h1>
+			<Box bg='green' w='100%' p={4} color='white'>
+				<h1>This is the Box</h1>
+
+				<ul>
+					<li>
+						<a href={url1}>[GO TO DASHBOARD]</a>
+					</li>
+					<li>
+						<a href={url2}>[GO TO SETTINGS]</a>
+					</li>
+				</ul>
 			</Box>
 		</div>
 	)
