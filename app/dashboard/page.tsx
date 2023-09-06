7f094cb5e45a39ad0d6ac325f766ec284bc73357
Yuliia Pavliuk <olga96855@gmail.com>
2023-09-06 16:23:59 +0300

    test#1

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,24 +1,13 @@
 // import Link from 'next/link';
-import React from 'react';
-
+import React from 'react'
 
 export default function Home() {
+	const myHome = '/'
 
-const myHome = '/';
-
-    return (
-      <div>
-        <h1>
-          Dashboard!
-        </h1>
-      
-   
-<a href={myHome}>
-[GO TO HOME]
-</a>
-  
-</div>
-  
-
- );
-  }
+	return (
+		<div>
+			<h1>Dashboard!</h1>
+			<a href={myHome}>[GO TO HOME]</a>
+		</div>
+	)
+}
