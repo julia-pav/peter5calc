cf377d5aacd74e297f056dc133c555cb4dc3731f
Yuliia Pavliuk <olga96855@gmail.com>
2023-09-06 16:21:54 +0300

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,25 @@
-import Link from 'next/link';
-import React from 'react';
+import Link from 'next/link'
+import React from 'react'
 
-import { Heading, Box, Divider } from "@chakra-ui/react";
+import { Heading, Box, Divider } from '@chakra-ui/react'
 
 export default function Home() {
+	const url1 = '/dashboard'
+	const url2 = '/settings'
+	return (
+		<div>
+			<Box bg='green' w='100%' p={4} color='white'>
+				<h1>This is the Box</h1>
 
-const url1 = '/dashboard';
-const url2 = '/settings'
-  return (
-    <div>
-
-<Box bg='tomato' w='100%' p={4} color='white'>
-This is the Box
-</Box>
-
-      <h1>
-        Hello!
-      </h1>
-   
-
-<a href={url1}>
-[GO TO DASHBOARD]
-</a>
-
-<a href={url2}>
-[GO TO SETTINGS]
-</a>
-
-
- </div>
-  );
+				<ul>
+					<li>
+						<a href={url1}>[GO TO DASHBOARD]</a>
+					</li>
+					<li>
+						<a href={url2}>[GO TO SETTINGS]</a>
+					</li>
+				</ul>
+			</Box>
+		</div>
+	)
 }
